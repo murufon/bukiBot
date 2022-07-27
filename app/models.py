490c179d6b58aa70e39d53e73436348f942e6a60
murufon <murufon2@gmail.com>
2022-07-27 20:00:55 +0900

    make models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class ServerConfig(models.Model):
+    server_name = models.CharField('サーバー名', max_length=200) # Discordのサーバー名は100文字まで
+    splatoon2_textchat_id = models.CharField('Splatoon2用テキストチャット', max_length=50) # DiscordのチャンネルIDは18文字
+    splatoon3_textchat_id = models.CharField('Splatoon3用テキストチャット', max_length=50) # DiscordのチャンネルIDは18文字
+    created_at = models.DateTimeField('作成日', auto_now_add=True)
+    updated_at = models.DateTimeField('更新日', auto_now=True)
+
+class Server(models.Model):
+    server_id = models.CharField('サーバーID', max_length=50) # DiscordのサーバーIDは18文字
+    config = models.OneToOneField(ServerConfig, verbose_name='サーバー設定', on_delete=models.CASCADE)
+    created_at = models.DateTimeField('作成日', auto_now_add=True)
+    updated_at = models.DateTimeField('更新日', auto_now=True)
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,10 +77,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("MYSQL_DATABASE"),
+        'USER': os.environ.get("MYSQL_USER"),
+        'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
+        'HOST': 'db',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 
 # Password validation
